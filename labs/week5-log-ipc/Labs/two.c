05875accc24feb46cd116d1e1a75582c42f9846d
Barnet Obeka <oworibarnet@gmail.com>
2023-02-24 11:27:47 +0000

    two completed

---
+++ labs/week5-log-ipc/Labs/two.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void)
+{
+    int fd[2], nbytes;
+    pid_t pid;
+    char string[]= "Your Child!!!!\n";
+    char readbuffer[100];
+
+	pipe(fd);
+	pid = fork();
+    if(pid < -1)
+    {
+        perror("Something went wrong with fork");
+        return 1;
+    }
+    if(pid == 0)
+    {
+        close(fd[0]);
+        write(fd[1], string, (strlen(string)+1));
+    }
+    else
+    {
+        close(fd[1]);
+        nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
+        printf("Message from Child: %s\n", readbuffer);
+
+    }
+    return(0);
+}
